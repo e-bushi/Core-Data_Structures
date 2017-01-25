589d5f652305418ee3c2d84b6ee15f08224cd03d
Chris Mauldin <Chrislovemauldin@gmail.com>
2017-01-25 07:03:44 -0800

    Started Set class structure

---
+++ source/set_adt.py
@@ -0,0 +1,49 @@
+from hashtable import HashTable
+
+class Set(object):
+
+    def __init__(self, number_of_groups=2):
+        '''Initialize this set implementation with a Hash Table'''
+        self.groups = {}
+
+        self.total_num_of_group_elements = 0
+
+        for i in range(number_of_groups):
+            self.groups[i] = {}
+
+    def __repr__(self):
+        '''Return a string representation of set'''
+        return "Set: {!r}".format(self.groups)
+
+    def add_group(self, group):
+        '''Add a group within a particular set'''
+        #number of groups within the set
+        set_length = len(self.groups)
+
+        #adds a new group to the set
+        self.groups[set_length] = group
+
+    def add_element_to_group(self, key, element, b=True):
+        '''Add element to a specific group within the set'''
+        group = self.groups[key]
+        group[element] = b
+
+
+
+    def remove(self):
+        '''Remove a group within the set'''
+        pass
+
+
+    def contains(self):
+        '''Returns tuple with that has a boolean value
+        and key of the group that contains element sought after, if element
+        doesn't exist within the set than returns False'''
+        pass
+
+
+
+if __name__ == '__main__':
+    s = Set(4)
+    s.add_element_to_group(0, "Chris")
+    print(s.__repr__())
---
+++ source/set_test.py
@@ -0,0 +1,41 @@
+from set_adt import Set
+import unittest
+
+if not hasattr(unittest.TestCase, 'assertCountEqual'):
+    unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+
+class SetTest(unittest.TestCase):
+
+    def test_init(self):
+        s = Set()
+        assert len(s.groups) == 2
+        assert s.total_num_of_group_elements == 0
+
+
+
+
+    def test_add_group(self):
+        s = Set()
+        assert len(s.groups) == 2
+        s.add_group({})
+        assert len(s.groups) == 3
+
+
+
+    def test_add_element_to_group(self):
+        s = Set()
+        assert len(s.groups) == 2
+        s.add_element_to_group(0, "Chris")
+        assert s.groups[0]["Chris"] == True
+
+
+
+
+    def test_remove(self):
+        pass
+
+
+
+
+    def test_contains(self):
+        pass
